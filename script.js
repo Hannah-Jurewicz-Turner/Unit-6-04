3a2396164cafce9ce8bd268fb1d84764b54dda2b
HannahJT <>
2021-06-07 13:46:11 +0000

    I removed white spaces and semicolons.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 document.getElementById('button').addEventListener('click', calculateAreaOfTrapezoid)
 
-function calculateAreaOfTrapezoid() {
-  let intBase1 = parseInt  (document.getElementById('input-one').value )
-  let intBase2 = parseInt(document.getElementById('input-two').value )
+function calculateAreaOfTrapezoid () {
+  let intBase1 = parseInt(document.getElementById('input-one').value)
+  let intBase2 = parseInt(document.getElementById('input-two').value)
   let intHeight = parseInt(document.getElementById('input-three').value)
-  let baseTotal = intBase1 + intBase2;
+  let baseTotal = intBase1 + intBase2
   alert((baseTotal / 2) * intHeight)
 }
