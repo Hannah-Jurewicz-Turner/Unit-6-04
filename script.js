e9d2c89b0cf31e6551ae281326ffb688a9c562b0
HannahJT <>
2021-06-07 14:23:32 +0000

    changed let to const.

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 document.getElementById('button').addEventListener('click', calculateAreaOfTrapezoid)
 
 function calculateAreaOfTrapezoid () {
-  let intBase1 = parseInt(document.getElementById('input-one').value)
-  let intBase2 = parseInt(document.getElementById('input-two').value)
-  let intHeight = parseInt(document.getElementById('input-three').value)
-  let baseTotal = intBase1 + intBase2
+  const intBase1 = parseInt(document.getElementById('input-one').value)
+  const intBase2 = parseInt(document.getElementById('input-two').value)
+  const intHeight = parseInt(document.getElementById('input-three').value)
+  const baseTotal = intBase1 + intBase2
   alert((baseTotal / 2) * intHeight)
 }
